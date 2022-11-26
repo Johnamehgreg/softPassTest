ab0344ac201d065ae5366d7cf27a10e01fb3f8d5
azudi <jerryazubuike001@gmail.com>
2022-11-26 08:59:06 +0100

    pop-up-animation
    commit

--- src/components/widget/ModalParentModule.tsx
+++ src/components/widget/ModalParentModule.tsx
@@ -22,10 +22,15 @@ function ModalParentModule(props: Props) {
         closeDropDown(event);
       }}
       ref={ref}
-      className="fixed z-50 top-0 left-0 w-full h-full bg-black/40 flex justify-center sm:items-center items-end"
+      className="fixed z-50 top-0 left-0 w-full h-full bg-black/40
+       flex justify-center sm:items-center items-end routes-container"
     >
-      <div className="bg-white rounded-md max-w-[600px] sm:min-w-[250px] w-full sm:w-auto sm:min-h-[100px] sm:max-h-[100vh] sm:h-auto h-[70vh] flex-none overflow-y-auto">
-        {children}
+      <div
+        className="bg-white rounded-md max-w-[600px] sm:min-w-[250px]
+       w-full sm:w-auto sm:min-h-[100px] sm:max-h-[100vh] sm:h-auto h-[70vh]
+        flex-none overflow-y-auto"
+      >
+        { children }
       </div>
     </div>
   );
